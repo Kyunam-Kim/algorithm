7145abeffa702a85b5d4731dc294d706320e292a
kim-kau-nam <rlarbska3824@gmail.com>
2020-01-03 03:58:12 +0900

    modified

--- doubleLinkedList.py
+++ doubleLinkedList.py
@@ -7,14 +7,15 @@ class Node:
 def init_list():
     global node_A
     node_A = Node("A")
-    node_B = Node("A")
-    node_D = Node("A")
-    node_E = Node("A")
+    node_B = Node("B")
+    node_D = Node("D")
+    node_E = Node("E")
     node_A.next = node_B
-    node_B.prev = node_A
     node_B.next = node_D
-    node_D.prev = node_B
+    node_B.prev = node_A
     node_D.next = node_E
+    node_D.prev = node_B
+
 
 def insert_node(data):
     global node_A
@@ -29,11 +30,31 @@ def insert_node(data):
     new_node.prev = node_P
     node_T.prev = new_node
 
+# def delete_node(del_data):
+#     global node_A
+#     pre_node = node_A
+#     next_node = pre_node.next
+#     next_next_node = next_node.next
+
+#     if pre_node.data == del_data:
+#         node_A = next_node
+#         del pre_node
+#         return
+
+#     while next_node:
+#         if next_node.data == del_data:
+#             next_next_node = next_node.next
+#             pre_node.next = next_node.next
+#             next_next_node.prev = next_node.prev
+#             del next_node
+#             break
+# 수정필요
+
 def print_list():
     global node_A
     node = node_A
     while node:
-        print(node)
+        print(node.data)
         node = node.next
     print
 
@@ -41,3 +62,9 @@ if __name__ == '__main__':
     print("Link")
     init_list()
     print_list()
+    print("Insert C")
+    insert_node("C")
+    print_list()
+    # print("Delete D")
+    # delete_node("D")
+    # print_list()
