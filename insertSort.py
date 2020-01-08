d6507da69c71b53064d4efd5aab6e3c1fbbc8a7e
kim-kau-nam <rlarbska3824@gmail.com>
2020-01-08 21:17:44 +0900

    modified

--- insertSort.py
+++ insertSort.py
@@ -1,26 +1,34 @@
-# import random
-# import time
+import random
+import time
 
-# compare_counter = 0
-# swap_counter = 0
+compare_counter = 0
+swap_counter = 0
 
-# def insertion_sort(my_list):
-#     global compare_counter, swap_counter
-#     my_list.insert(0, -1)
-#     for idx in range(2, len(my_list)):
-#         temp = my_list[idx]
-#         while my_list[idx] > temp:
-#             my_list[idx] = my_list[idx - 1] 
-#             idx = idx - 1
-#     del my_list[0]
+def insertion_sort(my_list):
+    global compare_counter, swap_counter
+    my_list.insert(0, -1)
+    for s_idx in range(2, len(my_list)):
+        temp = my_list[s_idx]
+        ins_idx = s_idx
+        compare_counter += 1
+        while my_list[ins_idx - 1] > temp:
+            swap_counter += 1
+            my_list[ins_idx] = my_list[ins_idx - 1] 
+            ins_idx = ins_idx - 1
+        
+        my_list[ins_idx] = temp
+    del my_list[0]
 
-# if __name__=='__main__':
-#     list = []
-#     for i in range(10):
-#         list.append(random.randint(1, 10))
-#     print("<Before Sort>")
-#     print(list)
-#     insertion_sort(list)
-#     print("<After sort")
-#     print(list)
-#  수정필요
+if __name__=='__main__':
+    list = []
+    input_n = input("input the data: ")
+    for i in range(int(input_n)):
+        list.append(random.randint(1, int(input_n)))
+    print("<Before Sort>")
+    print(list)
+
+    start_time = time.time()
+    insertion_sort(list)
+    running_time = time.time() - start_time
+    print("<After sort>")
+    print(list)
