76b41b7deb210b5c717af07e3986940b75374768
kim-kau-nam <rlarbska3824@gmail.com>
2020-01-15 23:51:37 +0900

    modified

--- shellSort.py
+++ shellSort.py
@@ -3,4 +3,13 @@ import time
 
 def shell_sort(random_list):
     h = 1
-    
+    while h < len(random_list):
+        h = h*3 + 1
+    h = h//3
+
+    # while h > 0:
+    #     for i in range(h):
+    #         start_index = i + h
+
+    #         while start_index < len(random_list):
+    # 수정 필요
