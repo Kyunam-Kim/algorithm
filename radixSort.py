0018242f5943188d92cac69f9d4946833cab24b9
kim-kau-nam <rlarbska3824@gmail.com>
2020-01-18 23:31:34 +0900

    modified

--- radixSort.py
+++ radixSort.py
@@ -3,3 +3,7 @@ from random import randint
 
 def get_digit(number, base, pos):
     return (number//base**pos) % base
+
+# def radixsort(1, base = 10):
+#     passes = int(log10(max(1))+1)
+#     output = [0]*len(1)
